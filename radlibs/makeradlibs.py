41ed7e68ed7c6319f07f36c040d062ef0af15d08
Stephanie <s.kirmer@gmail.com>
2019-05-07 14:09:15 -0500

    fixed some minor bugs

--- radlibs/makeradlibs.py
+++ radlibs/makeradlibs.py
@@ -5,6 +5,7 @@ import pandas as pd
 import feather
 import random
 import re
+import os
 from radlibs.generatewordopts import _generateWordOptions
 from radlibs.postagger import posTagger
 
@@ -13,15 +14,18 @@ def makeRadlibs(phrase, wordset = 'NA'):
     word_types = _generateWordOptions()
 
     if (wordset == 'NA'):
-        path = 'humor_dataset.feather'
-        wordset = feather.read_dataframe(path)
+        this_dir, this_filename = os.path.split(__file__)
+        DATA_PATH = os.path.join(this_dir, "data", "humor_dataset.feather")
+        wordset = feather.read_dataframe(DATA_PATH)
         wordset = wordset[wordset['mean'] > 1.2]
 
         wordset = posTagger(wordset)
         wordset['pos'] = wordset['pos'].str.lower()
 
-        path = 'proper_nouns.feather'
-        propernouns = feather.read_dataframe(path)
+        this_dir, this_filename = os.path.split(__file__)
+        DATA_PATH = os.path.join(this_dir, "data", "proper_nouns.feather")
+
+        propernouns = feather.read_dataframe(DATA_PATH)
 
         propernouns.columns = ['word', 'pos']
         wordset = pd.concat([wordset, propernouns], sort=False)
@@ -34,7 +38,7 @@ def makeRadlibs(phrase, wordset = 'NA'):
         word_types[key]['count'] = len(regex.findall(phrase))
 
         word_types[key]['sample'] = random.sample(
-            wordset[wordset['pos'].isin(descriptors)]['word'],
+            list(wordset[wordset['pos'].isin(descriptors)]['word']),
             word_types[key]['count']
         )
 
@@ -47,9 +51,8 @@ def makeRadlibs(phrase, wordset = 'NA'):
 
         if (count > 0):
             for word in sample:
-                new_phrase.replace(regex, word, 1) 
+                new_phrase = re.sub(regex, word, new_phrase, count = 1)#new_phrase.replace(regex, word, 1) 
         else:
             next
-    
-
-    return(new_phrase)
+            
+    return(new_phrase.capitalize())
--- radlibs/postagger.py
+++ radlibs/postagger.py
@@ -3,10 +3,13 @@
 
 import pandas as pd
 import feather
+import os 
 
 def posTagger(wordDF):
-    path = 'pos_lexicon.feather'
-    wordset = feather.read_dataframe(path)
+
+    this_dir, this_filename = os.path.split(__file__)
+    DATA_PATH = os.path.join(this_dir, "data", "pos_lexicon.feather")
+    wordset = feather.read_dataframe(DATA_PATH)
 
     tagged_wordDF = pd.merge(wordset, wordDF, on='word', how = 'inner')
 
